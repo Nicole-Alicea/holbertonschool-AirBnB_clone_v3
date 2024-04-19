b859e9c71305f31613f5129636e3ce077542fca7
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-04-19 13:24:23 -0400

    task 6 fixing

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,11 +12,11 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
-@app.teardown_appcontext
-def teardown(exception):
-    '''Performs cleanup operations after each request'''
-    
-    storage.close()
+# @app.teardown_appcontext
+# def teardown(exception):
+#    '''Performs cleanup operations after each request'''
+#    
+#    storage.close()
 
 
 @app.errorhandler(404)
