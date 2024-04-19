e6881aa62bbbee550a38a4024b57a345f85d58d0
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-04-19 19:24:50 -0400

    fixing pycodestyle

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -31,8 +31,9 @@ def get_state(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'],
-                strict_slashes=False)
+@app_views.route('/states/<state_id>',
+                 methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
     '''Deletes a State object'''
 
